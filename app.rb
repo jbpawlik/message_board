71d9a3aa3d33848324cbdb96bf5dbf62eb2ded92
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-22 14:00:18 -0700

    add integration spec to add album; spec passing
    Co-Authored-By: Kyle Williamson <kylewilliamsonv1@gmail.com>

--- app.rb
+++ app.rb
@@ -5,6 +5,22 @@ require('./lib/board')
 require('pry')
 also_reload('lib/**/*.rb')
 
+get('/') do
+  redirect to('/boards')
+end
+
 get('/boards') do
+  @boards = Board.all()
   erb(:boards)
+end
+
+get('/boards/new') do
+  erb(:new_board)
+end
+
+post('/boards') do
+  name = params[:name]
+  board = Board.new({:name => name, :id => nil, :timestamp => Time.new})
+  board.save
+  redirect to('/boards')
 end
--- spec/board_integration_spec.rb
+++ spec/board_integration_spec.rb
@@ -6,8 +6,18 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('create an path', {:type => :feature}) do
-  it('creates an album and then goes to the album page') do
-    visit('/boards')
-    expect(page).to have_content('test')
+  it('goes to the home page') do
+    visit('/')
+    expect(page).to have_content('Shrimp Talk')
+  end
+end
+
+describe('create a new board', {:type => :feature}) do
+  it('creates a new board and then goes to the boards page') do
+    visit('/')
+    click_on('Add a New Board')
+    fill_in('name', :with => 'Shrimp Biscuit')
+    click_on('Get Surfin')
+    expect(page).to have_content('Shrimp Biscuit')
   end
 end
